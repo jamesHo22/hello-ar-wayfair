b6e87f63cf71c5495490f1029a6fd2ccab623d35
jamesho <8.james.ho@gmail.com>
2018-11-03 21:19:37 -0400

    Implement dominant colors

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070026;
-        public static final int end = 0x7f07003a;
-        public static final int gone = 0x7f070044;
-        public static final int invisible = 0x7f07004d;
-        public static final int left = 0x7f07004f;
-        public static final int packed = 0x7f07005e;
-        public static final int parent = 0x7f07005f;
-        public static final int percent = 0x7f070061;
-        public static final int right = 0x7f070065;
-        public static final int spread = 0x7f070081;
-        public static final int spread_inside = 0x7f070082;
-        public static final int start = 0x7f070087;
-        public static final int top = 0x7f070094;
-        public static final int wrap = 0x7f07009b;
+        public static final int bottom = 0x7f070027;
+        public static final int end = 0x7f07003b;
+        public static final int gone = 0x7f070045;
+        public static final int invisible = 0x7f07004f;
+        public static final int left = 0x7f070051;
+        public static final int packed = 0x7f070060;
+        public static final int parent = 0x7f070061;
+        public static final int percent = 0x7f070063;
+        public static final int right = 0x7f070068;
+        public static final int spread = 0x7f070084;
+        public static final int spread_inside = 0x7f070085;
+        public static final int start = 0x7f07008a;
+        public static final int top = 0x7f070097;
+        public static final int wrap = 0x7f07009e;
     }
     public static final class styleable {
         private styleable() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/org/opencv/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/org/opencv/R.java
@@ -20,7 +20,7 @@ public final class R {
 
         public static final int any = 0x7f070020;
         public static final int back = 0x7f070022;
-        public static final int front = 0x7f070041;
+        public static final int front = 0x7f070042;
     }
     public static final class styleable {
         private styleable() {}
